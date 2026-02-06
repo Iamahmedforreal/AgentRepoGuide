c7f60183695a6ebcf6b04618758a862f2b55c355
Ahmeddaud11 <shahwah3344@gmail.com>
2026-02-06 18:36:06 +0300

    server set up for more

--- src-node/routes/authRoute.js
+++ src-node/routes/authRoute.js
@@ -3,7 +3,9 @@ const router = express.Router();
 import { clerkWebhookHandler } from "../controller/ControllerAuth.js";
 
 // Webhook route for Clerk events
-router.post("/webhooks", 
+router.post("/webhooks/clerk", 
     express.raw({ type: "application/json" }),
     clerkWebhookHandler
 );
+
+export default router;
--- src-node/server.js
+++ src-node/server.js
@@ -1,19 +1,26 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import authRoute from './routes/authRoute.js';
 dotenv.config();
 
 const app = express();
 
-
+const NODE_ENV = process.env.NODE_ENV || 'development';
 const PORT = process.env.PORT || 3000;
-
 app.use(express.json());
+
+// body parser middleware with size limit
+app.use(express.json({ limit: '10kb' }));
+app.use(express.urlencoded({ limit: '10kb', extended: true }));
 
 
 app.get('/', (req, res) => {
     res.send('Hello, World!');
 })
 
+//api routes
+app.use('/api', authRoute);
+
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT} in ${NODE_ENV} mode`);
 })
