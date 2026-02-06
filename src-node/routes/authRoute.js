afe18fb03fa310fd164e53e9927986658441c6d2
Ahmeddaud11 <shahwah3344@gmail.com>
2026-02-06 18:56:08 +0300

    fixed typo for webhook event enpoint

--- src-node/routes/authRoute.js
+++ src-node/routes/authRoute.js
@@ -3,7 +3,7 @@ const router = express.Router();
 import { clerkWebhookHandler } from "../controller/ControllerAuth.js";
 
 // Webhook route for Clerk events
-router.post("/webhooks/clerk", 
+router.post("/webhook/clerk", 
     express.raw({ type: "application/json" }),
     clerkWebhookHandler
 );
