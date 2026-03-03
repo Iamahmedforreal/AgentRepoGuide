6d8ace202fc7742618aecf0a6bcfb119a3c2d8be
Ahmeddaud11 <shahwah3344@gmail.com>
2026-03-03 16:41:30 +0300

    fixing the code

--- src-node/controller/urlController.js
+++ src-node/controller/urlController.js
@@ -1,9 +1,14 @@
 import { AppError } from '../utils/AppError';
 import urlService from '../service/urlService';
+import { error } from 'console';
 
 export const getUserurls = async (req, res) => {
     try{
         const { url } = req.body;
+        if(!url || typeof url !== 'string'){
+            throw new AppError('Invalid URL provided', 400);
+        }
+        
         const metadata = await urlService.parseGithubUrl(url);
         const savedUrl = await urlService.saveUrl(metadata, req.user.id);
         res.status(200).json({ success: true, data: savedUrl });
