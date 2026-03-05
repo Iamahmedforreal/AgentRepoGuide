7bb75659b80e55b61a4610204240c0daab1ac040
Ahmeddaud11 <shahwah3344@gmail.com>
2026-03-05 23:39:46 +0300

    comment1

--- src-node/controller/urlController.js
+++ src-node/controller/urlController.js
@@ -1,20 +1,17 @@
 import { AppError } from '../utils/AppError.js';
 import urlService from '../service/urlService.js';
 
-
+// Controller function to handle URL submissions and metadata saving
 export const getUserurls = async (req, res, next) => {
     try{
         const { url } = req.body;
         if(!url || typeof url !== 'string'){
             throw new AppError('Invalid URL provided', 400);
-        }
-        
+        }   
         const metadata = await urlService.parseGithubUrl(url);
         const savedUrl = await urlService.saveUrl(metadata, req.user.id);
         res.status(200).json({ success: true, data: savedUrl });
     }catch(err){
         next(err);
     }
-    
-
 }
