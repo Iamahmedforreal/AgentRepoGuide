80f03cf44e3a0f672544c290b785a160afc309a9
Ahmeddaud11 <shahwah3344@gmail.com>
2026-03-11 03:58:58 +0300

    queue for repo cloning

--- src-node/controller/urlController.js
+++ src-node/controller/urlController.js
@@ -5,13 +5,13 @@ import urlService from '../service/urlService.js';
 export const getUserurls = async (req, res, next) => {
     try{
         const { url } = req.body;
-        
-
-         
+        const userId = req.user.id;
         const metadata = await urlService.parseGithubUrl(url);
-        const savedUrl = await urlService.saveUrl(metadata, req.user.id);
+        const savedUrl = await urlService.saveUrl(metadata, userId);
         res.status(200).json({ success: true, data: savedUrl });
     }catch(err){
         next(err);
     }
 }
+
+
